47ec7c6621297b6e7c65cea0564330a945d6c973
Suraz Subedi <surazsubedi28@gmail.com>
2025-05-15 13:34:10 +0545

    update-Image_link

--- frontend/src/components/Cards/EmptyCard.jsx
+++ frontend/src/components/Cards/EmptyCard.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import image from "../../assets/images/empty_data_icon_149938.png";
 
 const EmptyCard = ({ message }) => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
       <img
-        src="./assets/images/empty_data_icon_149938.png"
+        src={image}
         alt="No Notes"
-        className="w-60 h-50 text-gray-300"
+        className="w-60 h-50 text-gray-300 bg-gray-700 rounded-3xl"
       />
       <p className="w-1/2 text-sm font-medium text-slate-700 text-center leading-7 mt-5">
         {message}
