0690f667e9fcde06f4f844e396385a476abe4b90
Suraz Subedi <surazsubedi28@gmail.com>
2025-05-16 09:39:37 +0545

    update

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://makemynotesss.netlify.app",
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
